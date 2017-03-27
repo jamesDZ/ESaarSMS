c0b3995794be6c5f27cafd3c60bdae9da5636d29
Alok_iOS_Broc <Alok_iOS_Broc@apples-macbook-pro.local>
2017-03-27 11:07:59 +0530

    Initial Commit

---
+++ SMS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMS
+//
+//  Created by Alok_iOS_Broc on 3/27/17.
+//  Copyright © 2017 E-SaarTechy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
