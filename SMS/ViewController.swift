28ad3fb649116ed5e1769d211940e2c1ec0351fe
Alok_iOS_Broc <Alok_iOS_Broc@apples-macbook-pro.local>
2017-05-26 13:14:31 +0530

    Tableview Logic

--- SMS/ViewController.swift
+++ SMS/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    let section = ["UserDetails", "Attendence", "Report"]
+    
+    let items = [["Margarita", "BBQ Chicken", "Peproni"], ["sausage", "meat lovers", "veggie lovers"], ["sausage", "chicken pesto", "prawns & mashrooms"]]
+
+    
     @IBOutlet weak var tblUserDetails: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,15 +22,26 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
         
-        cell.textLabel?.text = "";
+        cell.textLabel?.text = items[indexPath.section][indexPath.row]
         
         return cell
     }
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return self.section[section]
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return section.count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.section[section]
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //<#code#>
-        return 0
+        return items[section].count
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        // <#code#>
