b7cd8da0126e5a18c8610323596035cb93a49887
Alok_iOS_Broc <Alok_iOS_Broc@apples-macbook-pro.local>
2017-05-26 12:57:07 +0530

    File for Home screen and UI

--- SMS/ViewController.swift
+++ SMS/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tblUserDetails: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        cell.textLabel?.text = "";
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //<#code#>
+        return 0
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // <#code#>
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
